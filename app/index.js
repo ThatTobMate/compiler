1ca67ab33d836da7fb5b423ed8aa3871e4203ec9
Tobias Hale <thale.android@gmail.com>
2016-10-19 18:06:10 +0100

    compiler

--- app/index.js
+++ app/index.js
@@ -8,6 +8,11 @@ function readFiles (fileName) {
 	    var hasRequire = /require/.test(contents)
 	    // var bundle = new Function ('var modules = {}; var blah = true');
 	    // Redefined what require is? Pass it into the new function
+	    // function require (path) {
+	    // 	console.log('cache ', cache[path])
+	    // }
+	    // contents + require
+	    // console.log(contents + require + cache)
 	    cache[fileName] = new Function (contents)
 	    if (hasRequire) {
 	    	var matched = contents.match(/require((.*))/)[1]
@@ -24,7 +29,7 @@ function createFunction() {
 	return (function(){
 		var modules = {};
 		var uninitializedModules = {};
-		console.log('modules ', modules)
+		console.log(cache)
 		Object.keys(cache).forEach(function(file) {
 			modules[file] = cache[file]()
 		})
